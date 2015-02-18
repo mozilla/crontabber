dd819ff80838ab85d1f655570a61f0bd3f3a9fe0
Peter Bengtsson <mail@peterbe.com>
2015-02-18 10:42:16 -0800

    backport incorporated

--- crontabber/base.py
+++ crontabber/base.py
@@ -5,7 +5,7 @@
 import collections
 import datetime
 import re
-from toposort import toposort_flatten
+import functools
 
 from datetimeutil import utc_now
 from configman import Namespace, RequiredConfig
@@ -19,6 +19,65 @@ class FrequencyDefinitionError(Exception):
 #==============================================================================
 class CircularDAGError(Exception):
     pass
+
+
+#==============================================================================
+
+# The following two functions have been plucked from
+# https://bitbucket.org/ericvsmith/toposort but modified so it works
+# in Python 2.6.
+
+def toposort(data):
+    """Dependencies are expressed as a dictionary whose keys are items
+and whose values are a set of dependent items. Output is a list of
+sets in topological order. The first set consists of items with no
+dependences, each subsequent set consists of items that depend upon
+items in the preceeding sets.
+"""
+
+    # Special case empty input.
+    if len(data) == 0:
+        return
+
+    # Copy the input so as to leave it unmodified.
+    data = data.copy()
+
+    # Ignore self dependencies.
+    for k, v in data.items():
+        v.discard(k)
+    # Find all items that don't depend on anything.
+    extra_items_in_deps = functools.reduce(
+        set.union, data.values()
+    ) - set(data.keys())
+
+    # Add empty dependences where needed.
+    data.update(dict((item, set()) for item in extra_items_in_deps))
+    while True:
+        ordered = set(item for item, dep in data.items() if len(dep) == 0)
+        if not ordered:
+            break
+        yield ordered
+        data = dict(
+            (item, (dep - ordered))
+            for item, dep in data.items()
+            if item not in ordered
+        )
+    if len(data) != 0:
+        raise ValueError(
+            'Cyclic dependencies exist among these items: {}'
+            .format(', '.join(repr(x) for x in data.items()))
+        )
+
+
+def toposort_flatten(data, sort=True):
+    """Returns a single list of dependencies. For any set returned by
+toposort(), those items are sorted and appended to the result (just to
+make the results deterministic)."""
+
+    result = []
+    for d in toposort(data):
+        result.extend((sorted if sort else list)(d))
+    return result
 
 
 #==============================================================================
